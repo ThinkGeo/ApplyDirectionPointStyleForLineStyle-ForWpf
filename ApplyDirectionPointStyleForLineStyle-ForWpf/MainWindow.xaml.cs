c83d788f47abc795c6afdc7b1ceba11ce352ca04
leeliu <leeliu@thinkgeo.com>
2019-11-08 18:29:31 +0800

    Add ApplyDirectionPointStyleForLineStyle sample.

---
+++ ApplyDirectionPointStyleForLineStyle-ForWpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+using ThinkGeo.MapSuite.Wpf;
+
+namespace ApplyDirectionPointStyleForLineStyle_ForWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var imageBytes = Convert.FromBase64String("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAFCAYAAABIHbx0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABOSURBVChTYzAxMSEKX758efulS5essMmBMFZBbPjKlSv/ofgo0FBfdHkUDpJighhoWAOyXhSD8GEkQw5fvXrVCV0ehYMPAw3YjTuMTBgA4r93NU8NzhcAAAAASUVORK5CYII=".Substring(22));
+            var geoImage = new GeoImage(new MemoryStream(imageBytes));
+
+            var lineStyle = new LineStyle(new GeoPen(GeoColors.Black, 16) { StartCap = DrawingLineCap.Round, EndCap = DrawingLineCap.Round }, new GeoPen(GeoColors.White, 13) { StartCap = DrawingLineCap.Round, EndCap = DrawingLineCap.Round });
+            lineStyle.RequiredColumnNames.Add("RECID");
+            lineStyle.DirectionPointStyle = new PointStyle(geoImage);
+            lineStyle.DrawingPointStyle += LineStyle_DrawingPointStyle;
+
+            var streetsShapeFileFeatureLayer = new ShapeFileFeatureLayer("AppData\\Austinstreets.shp");
+            streetsShapeFileFeatureLayer.DrawingQuality = DrawingQuality.HighQuality;
+            streetsShapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle = lineStyle;
+            streetsShapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            var layerOverlay = new LayerOverlay();
+            layerOverlay.Layers.Add(streetsShapeFileFeatureLayer);
+
+            mapView.Overlays.Add(layerOverlay);
+            mapView.MapUnit = GeographyUnit.DecimalDegree;
+            mapView.CurrentExtent = new RectangleShape(-97.76312662255097, 30.30168722494507, -97.75471521508027, 30.29566834791565);
+            mapView.Refresh();
+        }
+
+        private void LineStyle_DrawingPointStyle(object sender, DrawingPointStyleEventArgs e)
+        {
+            if (e.Line.ColumnValues.ContainsKey("RECID") && int.Parse(e.Line.ColumnValues["RECID"]) > 4800)
+            {
+                e.RotationAngle = 0;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# ApplyDirectionPointStyleForLineStyle-ForWpf
+# Apply DirectionPointStyle for LineStyle for Wpf
+
+### Description
+
+The Map Suite WPF ApplyDirectionPointStyleForLineStyle sample will guide you to how to draw directionPointStyle of lineStyle on map. This ApplyDirectionPointStyleForLineStyle sample supports Map Suite 10.0.0.0 and higher and will show you how to create a WPF application using Map Suite WPF components.
+
+Please refer to [Wiki](http://wiki.thinkgeo.com/wiki/map_suite_desktop_for_wpf) for the details.
+
+![Screenshot](https://github.com/ThinkGeo/ApplyDirectionPointStyleForLineStyle-ForWpf/blob/master/Screenshot.gif)
+
+### About the Code
+
+``` csharp
+var lineStyle = new LineStyle(new GeoPen(GeoColors.Black, 16) { StartCap = DrawingLineCap.Round, EndCap = DrawingLineCap.Round }, new GeoPen(GeoColors.White, 13) { StartCap = DrawingLineCap.Round, EndCap = DrawingLineCap.Round });
+lineStyle.RequiredColumnNames.Add("RECID");
+lineStyle.DirectionPointStyle = new PointStyle(geoImage);
+lineStyle.DrawingPointStyle += LineStyle_DrawingPointStyle;
+
+private void LineStyle_DrawingPointStyle(object sender, DrawingPointStyleEventArgs e)
+{
+    if (e.Line.ColumnValues.ContainsKey("RECID") && int.Parse(e.Line.ColumnValues["RECID"]) > 4800)
+    {
+        e.RotationAngle = 0;
+    }
+}
+```
+
+### Getting Help
+
+[Map Suite Desktop for Wpf Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_desktop_for_wpf)
+
+[Map Suite Desktop for Wpf Product Description](https://thinkgeo.com/ui-controls#desktop-platforms)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### About ThinkGeo
+
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
+
